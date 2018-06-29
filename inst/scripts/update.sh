b0c3c7c91f3cb89d92c4e3113133293437af67de
Sebastian Kopf <seb.kopf@gmail.com>
2018-06-28 19:53:09 -0600

    cleanup of obsolete files

--- inst/scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# This is the script to update on unix systems (linux/OS X)
-
-# make sure we're running from the directory where the script is located
-SOURCE="${BASH_SOURCE[0]}"
-DIR="$( dirname "$SOURCE" )"
-cd "$DIR"
-
-Rscript -e "labwareC3::update_package(); Sys.sleep(5)"
