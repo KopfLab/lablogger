0ca82a18b351487ddb981d75f3356ef5392e27fc
Sebastian Kopf <seb.kopf@gmail.com>
2018-06-28 19:48:43 -0600

    write devices

--- R/db_write.R
+++ R/db_write.R
@@ -1,28 +1,28 @@
-#' Add a new owner record
-#' @param owner_id the unique identifier/name of the owner. Will error if id already exists.
-#' @param desc the description for the device owner (optional)
+#' Add a new group record
+#' @param group_id the unique identifier/name of the group. Will error if id already exists.
+#' @param desc the description for the device group (optional)
 #' @export
-c3_add_owner <- function(owner_id, desc = NA, con = default(con), quiet = default(quiet)) {
+c3_add_group <- function(group_id, desc = NA, con = default(con), quiet = default(quiet)) {
   con <- validate_db_connection(enquo(con))
-  if (!quiet) glue("\nInfo: add new owner '{owner_id}'... ") %>% message(appendLF = FALSE)
-  data <- data_frame(owner_id = owner_id, owner_desc = desc)
-  run_insert_sql(data, "owners", con, quiet = quiet)
+  if (!quiet) glue("\nInfo: add new group '{group_id}'... ") %>% message(appendLF = FALSE)
+  data <- data_frame(group_id = group_id, group_desc = desc)
+  run_insert_sql(data, "groups", con, quiet = quiet)
   return(invisible(data));
 }
 
 #' Add new device
-#' @param device_name device name / id, must be unique for each owner (will error if not)
+#' @param device_name device name / id, must be unique for each group (will error if not)
 #' @param desc device description
 #' @param device_type_id device type name, must exist in database (will error if not)
-#' @param owner_id owner name, must exist in database (will error if not)
+#' @param group_id group name, must exist in database (will error if not)
 #' @param particle_id optional, will be automatically filled in the first time the device logs to the database
 #' @param in_use whether device is in use (if not, cannot log any data)
 #' @export
-c3_add_device <- function(device_name, desc = NA, device_type_id = "undefined", owner_id = default(owner_id), particle_id = NA, in_use = TRUE, con = default(con), quiet = default(quiet)) {
+c3_add_device <- function(device_name, desc = NA, device_type_id = "undefined", group_id = default(group_id), particle_id = NA, in_use = TRUE, con = default(con), quiet = default(quiet)) {
   con <- validate_db_connection(enquo(con))
-  if (!quiet) glue("\nInfo: add new device '{device_name}' for owner '{owner_id}'... ") %>%
+  if (!quiet) glue("\nInfo: add new device '{device_name}' for group '{group_id}'... ") %>%
     message(appendLF = FALSE)
-  data <- data_frame(device_name, device_desc = desc, device_type_id, owner_id, particle_id, in_use)
+  data <- data_frame(device_name, device_desc = desc, device_type_id, group_id, particle_id, in_use)
   run_insert_sql(data, "devices", con, quiet = quiet)
   return(invisible(data));
 }
