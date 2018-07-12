08316f22e5c9ccc744a7953c76e490b0e050b3e0
Sebastian Kopf <seb.kopf@gmail.com>
2018-07-12 14:56:24 -0600

    additional function imports

--- R/package.R
+++ R/package.R
@@ -1,23 +1,23 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @importFrom rlang quo quos enquo !! !!! is_quosure quo_expr quo_text quo_is_null quo_is_symbol quo_is_lang lang_head sym eval_tidy
+#' @importFrom rlang quo quos enquo !! !!! is_quosure quo_expr quo_text quo_is_null quo_is_symbol quo_is_lang lang_head sym eval_tidy parse_expr
 #' @importFrom curl new_handle curl_fetch_memory
 #' @importFrom jsonlite fromJSON toJSON
 #' @importFrom glue glue collapse
-#' @importFrom dplyr select rename filter as_data_frame %>% tbl collect mutate data_frame everything starts_with ends_with left_join arrange
-#' @importFrom ggplot2 ggplot aes geom_point geom_line theme_bw theme_void facet_grid scale_x_datetime labs annotate
-#' @importFrom lubridate with_tz force_tz now ymd_hms as.duration
-#' @importFrom purrr map map_lgl map_chr map_dbl map2 map2_chr walk
+#' @importFrom dplyr select rename filter as_data_frame %>% tbl collect mutate data_frame everything starts_with ends_with left_join arrange group_by do summarize bind_rows between
+#' @importFrom ggplot2 ggplot aes geom_point geom_line theme_bw theme_void facet_grid scale_x_datetime labs annotate %+% ggsave theme element_text
+#' @importFrom lubridate with_tz force_tz now ymd_hms as.duration as_datetime
+#' @importFrom purrr map map_lgl map_chr map_dbl map2 map2_chr walk walk2
 #' @importFrom stringr str_c str_replace fixed str_interp
 #' @importFrom readr write_rds read_rds
 #' @importFrom DBI dbExecute
 #' @importFrom pool dbPool
 #' @importFrom RPostgreSQL PostgreSQL
-#' @importFrom tidyr gather
+#' @importFrom tidyr gather nest unnest
 #' @import shiny
 #' @import shinydashboard
-#' @importFrom shinyjs useShinyjs hidden show hide toggle
+#' @importFrom shinyjs useShinyjs hidden show hide toggle disabled toggleState
 #' @importFrom shinyAce aceEditor
 #' @importFrom shinyBS bsTooltip
 #' @importFrom shinycssloaders withSpinner
